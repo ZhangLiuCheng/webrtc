2fe83010aff104071922a737363f69b0ae698828
zhangliucheng <414616504@qq.com>
2020-01-16 15:27:30 +0800

    update

--- app/src/main/java/com/tech/playinsdk/webrtc/GenymotionActivity.java
+++ app/src/main/java/com/tech/playinsdk/webrtc/GenymotionActivity.java
@@ -3,13 +3,11 @@ package com.tech.playinsdk.webrtc;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.tbruyelle.rxpermissions2.RxPermissions;
 
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
@@ -44,169 +42,24 @@ import javax.net.ssl.X509TrustManager;
 
 public class GenymotionActivity extends AppCompatActivity implements View.OnTouchListener {
 
-    PeerConnectionFactory peerConnectionFactory;
-    PeerConnection peerConnection;
-    SurfaceViewRenderer remoteView;
-
     private ArrayBlockingQueue<String> sendQueue = new ArrayBlockingQueue<>(200);
+
+    private PeerConnectionFactory peerConnectionFactory;
+    private PeerConnection peerConnection;
+    private SurfaceViewRenderer remoteView;
+
     private Thread mWriteThread;
-    public class WriteThread extends Thread {
-        @Override
-        public void run() {
-            try {
-                while (!isInterrupted()) {
-                    String sendStr = sendQueue.take();
-                    client.send(sendStr);
-                }
-            } catch (Exception e) {
-                Log.e("TAG", "WriteThread send error： " + e);
-            }
-        }
-    }
-
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        int action = event.getAction();
-        int mode = -1;
-        if (action == MotionEvent.ACTION_DOWN) {
-            mode = 0;
-        } else if (action == MotionEvent.ACTION_MOVE) {
-            mode = 2;
-        } else if (action == MotionEvent.ACTION_UP) {
-            mode = 1;
-        }
-        try {
-            JSONObject obj = new JSONObject()
-                    .putOpt("type", "MULTI_TOUCH")
-                    .putOpt("mode", mode)
-                    .putOpt("nb", event.getPointerCount());
-            JSONArray points = new JSONArray();
-            for (int i = 0; i < event.getPointerCount(); i++) {
-                points.put(new JSONObject()
-                        .putOpt("x", getXcoordinate(event.getX(i)))
-                        .putOpt("y", getYcoordinate(event.getY(i))));
-            }
-            obj.putOpt("points", points);
-            sendQueue.offer(obj.toString());
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return true;
-    }
-
-    private int destWidth = 360;
-    private int destHeight = 640;
-    private float getXcoordinate(float srcX) {
-        return srcX * destWidth / remoteView.getWidth();
-    }
-    private float getYcoordinate(float srcY) {
-        return srcY * destHeight / remoteView.getHeight();
-
-    }
-
-    private class MyWebSocketClient extends WebSocketClient {
-
-        public MyWebSocketClient(URI serverUri) {
-            super(serverUri);
-        }
-
-        @Override
-        public void onOpen(ServerHandshake handshakedata) {
-            Log.e("TAG", "onOpen");
-        }
-
-        @Override
-        public void onMessage(String message) {
-            Log.e("TAG", "onMessage " + message);
-            try {
-                JSONObject obj = new JSONObject(message);
-                if (obj.optString("code").equals("SUCCESS")) {
-                    sendToken();
-                } else if (obj.optString("connection").equals("SUCCESS")) {
-                    sendOffer();
-                } else if (obj.optString("type").equals("answer")) {
-                    receiveAnswer(message);
-                } else if (obj.has("candidate")){
-                    onIceCandidateReceived(message);
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-
-        @Override
-        public void onClose(int code, String reason, boolean remote) {
-            Log.e("TAG", "onClose " + code + " " + reason + " " + remote);
-        }
-
-        @Override
-        public void onError(Exception ex) {
-            Log.e("TAG", "onError " + ex.toString());
-        }
-    }
-
-    private void initWebSocketClient(WebSocketClient webSocketClient) {
-        try {
-            SSLContext sslContext = SSLContext.getInstance("TLS");
-            sslContext.init(null, new TrustManager[]{
-                    new X509TrustManager() {
-                        @Override
-                        public void checkClientTrusted(X509Certificate[] chain, String authType) {
-                        }
-                        @Override
-                        public void checkServerTrusted(X509Certificate[] chain, String authType) {
-                        }
-                        @Override
-                        public X509Certificate[] getAcceptedIssuers() {
-                            return new X509Certificate[0];
-                        }
-                    }
-            }, new SecureRandom());
-            SSLSocketFactory factory = sslContext.getSocketFactory();
-            webSocketClient.setSocketFactory(factory);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     private MyWebSocketClient client;
 
-    private void sendToken() {
-        JSONObject obj = new JSONObject();
-        try {
-            obj.put("type", "token");
-            obj.put("token", "i-0176bcb6ccb14bece");
-            client.send(obj.toString());
-            Log.e("TAG", "发送token成功");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
 
-//    @Override
-//    public boolean onKeyDown(int keyCode, KeyEvent event) {
-//        switch (keyCode) {
-//            case KeyEvent.KEYCODE_VOLUME_UP:
-//                mAudioManager.adjustStreamVolume(AudioManager.STREAM_VOICE_CALL, AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI);
-//                return true;
-//            case KeyEvent.KEYCODE_VOLUME_DOWN:
-//                mAudioManager.adjustStreamVolume(AudioManager.STREAM_VOICE_CALL, AudioManager.ADJUST_LOWER, AudioManager.FLAG_SHOW_UI);
-//                return true;
-//            default:
-//                break;
-//        }
-//        return super.onKeyDown(keyCode, event);
-//    }
-
-//    AudioManager mAudioManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_genymotion);
 
-//        mAudioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
-//        mAudioManager.setMode(AudioManager.MODE_IN_CALL);
-//        mAudioManager.setSpeakerphoneOn(false);
+        setVolumeControlStream(AudioManager.STREAM_VOICE_CALL);
+        AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        audioManager.setSpeakerphoneOn(true);
 
         init();
     }
@@ -240,10 +93,8 @@ public class GenymotionActivity extends AppCompatActivity implements View.OnTouc
         mWriteThread.start();
     }
 
-    MediaStream mediaStream;
-    EglBase.Context eglBaseContext;
     private void initWebrtc() {
-        eglBaseContext = EglBase.create().getEglBaseContext();
+        EglBase.Context eglBaseContext = EglBase.create().getEglBaseContext();
 
         remoteView = findViewById(R.id.remoteView);
         remoteView.setMirror(false);
@@ -258,6 +109,7 @@ public class GenymotionActivity extends AppCompatActivity implements View.OnTouc
             @Override
             public void onFrameResolutionChanged(int i, int i1, int i2) {
                 Log.e("TAG", "onFrameResolutionChanged " + i + " -- " + i1 + " -- " + i2);
+                runOnUiThread(() -> adapterSize(i, i1));
             }
         });
 
@@ -266,6 +118,7 @@ public class GenymotionActivity extends AppCompatActivity implements View.OnTouc
                 .builder(this)
                 .createInitializationOptions());
         PeerConnectionFactory.Options options = new PeerConnectionFactory.Options();
+        options.disableEncryption = false;
 
         DefaultVideoEncoderFactory defaultVideoEncoderFactory = new DefaultVideoEncoderFactory(eglBaseContext, true, true);
         DefaultVideoDecoderFactory defaultVideoDecoderFactory = new DefaultVideoDecoderFactory(eglBaseContext);
@@ -275,15 +128,7 @@ public class GenymotionActivity extends AppCompatActivity implements View.OnTouc
                 .setVideoEncoderFactory(defaultVideoEncoderFactory)
                 .setVideoDecoderFactory(defaultVideoDecoderFactory)
                 .createPeerConnectionFactory();
-        mediaStream = peerConnectionFactory.createLocalMediaStream("mediaStream");
-
-//        VideoSource videoSource = peerConnectionFactory.createVideoSource(true);
-//        VideoTrack videoTrack = peerConnectionFactory.createVideoTrack("100", videoSource);
-//        mediaStream.addTrack(videoTrack);
-
-//        AudioSource audioSource = peerConnectionFactory.createAudioSource(new MediaConstraints());
-//        AudioTrack audioTrack = peerConnectionFactory.createAudioTrack("audiotrack", audioSource);
-//        mediaStream.addTrack(audioTrack);
+        MediaStream mediaStream = peerConnectionFactory.createLocalMediaStream("mediaStream");
 
         List<PeerConnection.IceServer> iceServers = new ArrayList<>();
         iceServers.add(PeerConnection.IceServer.builder("stun:stun.genymotion.com:3478").createIceServer());
@@ -305,17 +150,44 @@ public class GenymotionActivity extends AppCompatActivity implements View.OnTouc
                 }
                 if (mediaStream.audioTracks.size() > 0) {
                     AudioTrack remoteAudioTrack = mediaStream.audioTracks.get(0);
-                    remoteAudioTrack.setVolume(20);
+                    remoteAudioTrack.setVolume(0.5);
                 }
             }
         });
         peerConnection.addStream(mediaStream);
     }
 
+    private void adapterSize(int remoteWidth, int remoteHeight) {
+        int width = remoteView.getWidth();
+        int height = remoteView.getHeight();
+        int destWidth;
+        int destHeight;
+        float scaleWidth = width * 1.0f / remoteWidth;
+        float scaleHeight = height * 1.0f / remoteHeight;
+        if (scaleWidth < scaleHeight) {
+            destWidth = width;
+            destHeight = (int) (width * 1.0f * remoteHeight / remoteWidth);
+        } else {
+            destWidth = (int) (height * 1.0f * remoteWidth / remoteHeight);
+            destHeight = height;
+        }
+        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) remoteView.getLayoutParams();
+        params.width = destWidth;
+        params.height = destHeight;
+        remoteView.setLayoutParams(params);
+    }
+
     private void sendOffer() {
         MediaConstraints constraints = new MediaConstraints();
-//        constraints.mandatory.add(new MediaConstraints.KeyValuePair("OfferToReceiveAudio", "true"));
+        constraints.mandatory.add(new MediaConstraints.KeyValuePair("OfferToReceiveAudio", "true"));
         constraints.mandatory.add(new MediaConstraints.KeyValuePair("OfferToReceiveVideo", "true"));
+//        constraints.mandatory.add(new MediaConstraints.KeyValuePair("DtlsSrtpKeyAgreement", "true"));
+//        constraints.mandatory.add(new MediaConstraints.KeyValuePair("maxWidth", "720"));
+//        constraints.mandatory.add(new MediaConstraints.KeyValuePair("maxHeight", "1280"));
+//        constraints.mandatory.add(new MediaConstraints.KeyValuePair("minWidth", "480"));
+//        constraints.mandatory.add(new MediaConstraints.KeyValuePair("minHeight", "640"));
+//        constraints.mandatory.add(new MediaConstraints.KeyValuePair("maxFrameRate", "1000"));
+//        constraints.mandatory.add(new MediaConstraints.KeyValuePair("minFrameRate", "1000"));
 
         peerConnection.createOffer(new SdpAdapter("local offer sdp") {
             @Override
@@ -359,7 +231,7 @@ public class GenymotionActivity extends AppCompatActivity implements View.OnTouc
         }
     }
 
-    private  void onIceCandidateReceived(String message) {
+    private void onIceCandidateReceived(String message) {
         try {
             JSONObject data = new JSONObject(message);
             Log.e("TAG", "onIceCandidateReceived ---> " + data.toString());
@@ -370,6 +242,151 @@ public class GenymotionActivity extends AppCompatActivity implements View.OnTouc
             ));
         } catch (Exception ex) {
             ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        int action = event.getAction();
+        int mode = -1;
+        if (action == MotionEvent.ACTION_DOWN) {
+            mode = 0;
+        } else if (action == MotionEvent.ACTION_MOVE) {
+            mode = 2;
+        } else if (action == MotionEvent.ACTION_UP) {
+            mode = 1;
+        }
+        try {
+            JSONObject obj = new JSONObject();
+            obj.putOpt("type", "MULTI_TOUCH")
+                    .putOpt("mode", mode)
+                    .putOpt("nb", event.getPointerCount());
+            JSONArray points = new JSONArray();
+            for (int i = 0; i < event.getPointerCount(); i++) {
+                points.put(new JSONObject()
+                        .putOpt("x", getXcoordinate(event.getX(i)))
+                        .putOpt("y", getYcoordinate(event.getY(i))));
+            }
+            obj.putOpt("points", points);
+            sendQueue.offer(obj.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+
+    private float getXcoordinate(float srcX) {
+        return srcX * destWidth / remoteView.getWidth();
+    }
+
+    private float getYcoordinate(float srcY) {
+        return srcY * destHeight / remoteView.getHeight();
+    }
+
+    public class WriteThread extends Thread {
+        @Override
+        public void run() {
+            try {
+                while (!isInterrupted()) {
+                    String sendStr = sendQueue.take();
+                    client.send(sendStr);
+                    Log.e("TAG", "send =========> " + sendQueue.size());
+                }
+            } catch (Exception e) {
+                Log.e("TAG", "WriteThread send error： " + e);
+            }
+        }
+    }
+
+
+    private int destWidth = 360;
+    private int destHeight = 640;
+
+    private class MyWebSocketClient extends WebSocketClient {
+
+        public MyWebSocketClient(URI serverUri) {
+            super(serverUri);
+//            super( serverUri, new Draft_6455(), null, 30000);
+        }
+
+        @Override
+        public void onOpen(ServerHandshake handshakedata) {
+            Log.e("TAG", "onOpen");
+//            try {
+//                Socket socket = getSocket();
+//                socket.setSendBufferSize(200 * 1024);
+//            } catch (SocketException e) {
+//                e.printStackTrace();
+//            }
+        }
+
+        @Override
+        public void onMessage(String message) {
+            Log.e("TAG", "onMessage " + message);
+            try {
+                JSONObject obj = new JSONObject(message);
+                if (obj.optString("code").equals("SUCCESS")) {
+                    sendToken();
+                } else if (obj.optString("connection").equals("SUCCESS")) {
+                    sendOffer();
+                } else if (obj.optString("type").equals("answer")) {
+                    receiveAnswer(message);
+                } else if (obj.has("candidate")) {
+                    onIceCandidateReceived(message);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onClose(int code, String reason, boolean remote) {
+            Log.e("TAG", "onClose " + code + " " + reason + " " + remote);
+        }
+
+        @Override
+        public void onError(Exception ex) {
+            ex.printStackTrace();
+            Log.e("TAG", "onError " + ex.toString());
+        }
+    }
+
+    private void initWebSocketClient(WebSocketClient webSocketClient) {
+        try {
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, new TrustManager[]{
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(X509Certificate[] chain, String authType) {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(X509Certificate[] chain, String authType) {
+                        }
+
+                        @Override
+                        public X509Certificate[] getAcceptedIssuers() {
+                            return new X509Certificate[0];
+                        }
+                    }
+            }, new SecureRandom());
+            SSLSocketFactory factory = sslContext.getSocketFactory();
+            webSocketClient.setSocketFactory(factory);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendToken() {
+        JSONObject obj = new JSONObject();
+        try {
+            obj.put("type", "token");
+            obj.put("token", "i-0176bcb6ccb14bece");
+            client.send(obj.toString());
+            Log.e("TAG", "发送token成功");
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
     }
 }
